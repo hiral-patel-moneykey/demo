508dc5927d6449ef6eca5111f4fa0c8c01109e6d
hpatel <hiral.patel@moneykey.com>
2018-06-07 10:31:32 -0400

    Changes to Integration test folder name. PhoneUtility Test file changes.

--- src/test/java/com/mk/cti/CtiApplicationTests.java
+++ src/test/java/com/mk/cti/CtiApplicationTests.java
@@ -1,15 +1,23 @@
 package com.mk.cti;
 
+import com.mk.cti.utility.PhoneUtility;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
 
 // @RunWith(SpringRunner.class)
 @SpringBootTest
 // @ActiveProfiles("test")
 public class CtiApplicationTests {
+
+
 
     @Test
     public void contextLoads() {
@@ -17,5 +25,4 @@ public class CtiApplicationTests {
         System.out.println("Unit Tests");
 
     }
-
 }
--- src/test/java/com/mk/cti/utility/PhoneUtilityTest.java
+++ src/test/java/com/mk/cti/utility/PhoneUtilityTest.java
@@ -8,18 +8,30 @@ import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
+import java.util.Arrays;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PhoneUtilityTest {
 
     @Autowired
-    PhoneUtility phoneUtility;
+    private PhoneUtility phoneUtility;
 
     @Test
     public void shouldReturnPhoneResponseObject() {
 
         if(phoneUtility == null)
             phoneUtility = new PhoneUtility();
+
         PhoneResponse phoneResponse = phoneUtility.getPhoneResponse("416123123");
         assertThat(phoneResponse, instanceOf(PhoneResponse.class));
 
