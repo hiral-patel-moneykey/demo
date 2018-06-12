713e1a94e130fe39e59cf1213fd97efbb4bac4cd
hpatel <hiral.patel@moneykey.com>
2018-06-12 10:58:50 -0400

    Added basic Java configuration to run react app in the build. This settings not working yet.

---
+++ src/main/java/com/mk/cti/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.mk.cti.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "public/index";
+    }
+
+}
