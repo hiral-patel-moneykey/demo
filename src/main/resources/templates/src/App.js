8d18a74ed299b9b2ef8494a34336658613b4f49b
hpatel <hiral.patel@moneykey.com>
2018-06-13 11:08:37 -0400

    Passing react environment variables from pom.xml file or env.local file.

--- src/main/resources/templates/src/App.js
+++ src/main/resources/templates/src/App.js
@@ -7,10 +7,12 @@ class App extends Component {
 
     constructor() {
         super();
-        console.log("REACT_APP_RANDOM "+ process.env.REACT_APP_RANDOM);
-        console.log("REACT_APP_CONFIG_ENV is "+ process.env.REACT_APP_CONFIG_ENV);
-        console.log(config.message);
-        console.log(config.baseurl);
+
+        console.log("REACT_APP_CONFIG_PROFILE "+ process.env.REACT_APP_CONFIG_PROFILE);
+        // console.log("REACT_APP_RANDOM "+ process.env.REACT_APP_RANDOM);
+        // console.log("REACT_APP_CONFIG_ENV is "+ process.env.REACT_APP_CONFIG_ENV);
+        console.log("Message is "+ config.message);
+        console.log("Base URL is "+ config.baseurl);
     }
 
   render() {
