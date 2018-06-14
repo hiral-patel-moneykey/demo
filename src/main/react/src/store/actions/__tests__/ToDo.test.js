ec94e9ce610ef9bb852230a3e702d99946a50fc4
hpatel <hiral.patel@moneykey.com>
2018-06-14 17:09:50 -0400

    Moved test to a __tests__ folder to showcase react's standard way of specifying a test.

--- src/main/react/src/store/actions/__tests__/ToDo.test.js
+++ src/main/react/src/store/actions/__tests__/ToDo.test.js
@@ -1,5 +1,5 @@
-import * as actionTypes from "./actionTypes";
-import * as actions from "../actions";
+import * as actionTypes from "../actionTypes";
+import * as actions from "../../actions";
 
 describe("To Do actions are created", () => {
 
